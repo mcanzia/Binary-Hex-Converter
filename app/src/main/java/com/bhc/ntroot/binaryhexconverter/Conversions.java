43840127c0ea97109899b15dd43bd695ec7c131d
mjcanzia <mjcanzia@ncsu.edu>
2017-07-18 20:18:26 -0400

    Update MainActivity file with buttons to conversion screens

--- app/src/main/java/com/bhc/ntroot/binaryhexconverter/Conversions.java
+++ app/src/main/java/com/bhc/ntroot/binaryhexconverter/Conversions.java
@@ -13,7 +13,6 @@ public class Conversions {
     public static String decimalToBinary(int num) {
         StringBuilder binary = new StringBuilder();
         if (num == 0) {
-            //System.out.println("0");
             return "0";
         }
         while (num > 0) {
@@ -24,14 +23,12 @@ public class Conversions {
             }
             num = num / 2;
         }
-        //System.out.println(binary.toString());
         return binary.toString();
     }
 
     public static String decimalToOctal(int num) {
         StringBuilder octal = new StringBuilder();
         if (num == 0) {
-            //System.out.println("0");
             return "0";
         }
         while (num > 0) {
@@ -42,14 +39,12 @@ public class Conversions {
             }
             num = num / 8;
         }
-        //System.out.println(octal.toString());
         return octal.toString();
     }
 
     public static String decimalToHex(int num) {
         StringBuilder hex = new StringBuilder();
         if (num == 0) {
-            //System.out.println("0");
             return "0";
         }
         while (num > 0) {
@@ -65,7 +60,64 @@ public class Conversions {
             }
             num = num / 16;
         }
-        //System.out.println(hex.toString());
         return hex.toString();
+    }
+
+    public static int binaryToDecimal(String binary) {
+        char bitList[] = binary.toCharArray();
+        int length = bitList.length;
+        int num = 0;
+        for (int i = 0; i <= length - 1; i++) {
+            if (bitList[i] == '1') {
+                num += Math.pow(2, length - 1 - i);
+            }
+        }
+        return num;
+    }
+
+    public static int octalToDecimal(int octal) {
+        int pow = 0;
+        int decimal = 0;
+        int size = Integer.toString(octal).length();
+        while(pow < size) {
+            int temp = (int) (octal % Math.pow(10, pow + 1));
+            temp = (int) (temp / Math.pow(10, pow));
+            decimal += temp * Math.pow(8, pow);
+            octal = octal - temp;
+            pow++;
+        }
+        return decimal;
+    }
+
+    public static int hexToDecimal(String hex) {
+        char bitList[] = hex.toCharArray();
+        int decimal = 0;
+        int pow = 0;
+        for (int i = bitList.length - 1; i >= 0; i--) {
+            //if (Character.isAlphabetic(bitList[i])) {
+            decimal += hexHelper(bitList[i]) * Math.pow(16, pow);
+            //} else {
+            //decimal += Character.getNumericValue(bitList[i]) * Math.pow(16, pow);
+            //}
+            pow++;
+        }
+        return decimal;
+    }
+
+    private static int hexHelper(char c) {
+        if (c == 'A') {
+            return 10;
+        } else if (c == 'B') {
+            return 11;
+        } else if (c == 'C') {
+            return 12;
+        } else if (c == 'D') {
+            return 13;
+        } else if (c == 'E') {
+            return 14;
+        } else if (c == 'F') {
+            return 15;
+        }
+        return Character.getNumericValue(c);
     }
 }
--- app/src/main/java/com/bhc/ntroot/binaryhexconverter/MainActivity.java
+++ app/src/main/java/com/bhc/ntroot/binaryhexconverter/MainActivity.java
@@ -1,139 +1,55 @@
 package com.bhc.ntroot.binaryhexconverter;
 
-import android.content.Context;
-import android.support.annotation.StringDef;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.Gravity;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout mainLayout;
-    private EditText seqInputContainer;
-    private TextView errorDisplay;
-    private TextView testDisplay1;
-    private TextView testDisplay2;
-    private TextView testDisplay3;
+    private Button dec;
+    private Button bin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        Button nextPage = (Button) findViewById(R.id.button);
-        nextPage.setOnClickListener(new View.OnClickListener() {
+        dec = (Button) findViewById(R.id.decButton);
+        dec.setX(0);
+        dec.setY(0);
+        dec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToSecondActivity();
+                goToDecimal();
             }
         });
-         */
 
-        mainLayout = (LinearLayout)findViewById(R.id.linearLayoutBack);
 
-        seqInputContainer = (EditText) findViewById(R.id.seqInput);
-        seqInputContainer.setX(20);
-        seqInputContainer.setY(150);
-
-        errorDisplay = (TextView)findViewById(R.id.error);
-        errorDisplay.setX(20);
-        errorDisplay.setY(300);
-
-        testDisplay1 = (TextView)findViewById(R.id.seqDisplay1);
-        testDisplay1.setX(20);
-        testDisplay1.setY(750);
-
-        testDisplay2 = (TextView)findViewById(R.id.seqDisplay2);
-        testDisplay2.setX(20);
-        testDisplay2.setY(1050);
-
-        testDisplay3 = (TextView)findViewById(R.id.seqDisplay3);
-        testDisplay3.setX(20);
-        testDisplay3.setY(1350);
-
-        Button convert;
-        convert = (Button) findViewById(R.id.button2);
-        convert.setX(20);
-        convert.setY(400);
-        convert.setOnClickListener(new View.OnClickListener() {
+        bin = (Button) findViewById(R.id.binButton);
+        bin.setX(0);
+        bin.setY(0);
+        bin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (checkIfValid()) {
-                    displayInput();
-                    errorDisplay.setText("");
-                } else {
-                    errorDisplay.setText("Invalid Sequence");
-                }
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
+                goToBinary();
             }
         });
+
     }
 
-    /*
-    private void goToSecondActivity() {
+    private void goToDecimal() {
 
-        Intent intent = new Intent(this, SecondActivity.class);
+        Intent intent = new Intent(this, Decimal.class);
         startActivity(intent);
 
     }
-    */
 
-    private void displayInput() {
-        final TextView tit1;
-        final TextView tit2;
-        final TextView tit3;
+    private void goToBinary() {
 
-        tit1 = (TextView)findViewById(R.id.title1);
-        tit1.setX(20);
-        tit1.setY(600);
-        tit1.setTextSize(25);
-        tit2 = (TextView)findViewById(R.id.title2);
-        tit2.setX(20);
-        tit2.setY(900);
-        tit2.setTextSize(25);
-        tit3 = (TextView)findViewById(R.id.title3);
-        tit3.setX(20);
-        tit3.setY(1200);
-        tit3.setTextSize(25);
+        Intent intent = new Intent(this, Binary.class);
+        startActivity(intent);
 
-        tit1.setText("Binary");
-        tit2.setText("Octal");
-        tit3.setText("Hexadecimal");
-
-
-        //Conversions
-        int tempInput = Integer.parseInt(seqInputContainer.getText().toString());
-
-        String convertToBinary = Conversions.decimalToBinary(tempInput);
-        testDisplay1.setText(convertToBinary);
-        testDisplay1.setTextSize(15);
-
-        String convertToOctal = Conversions.decimalToOctal(tempInput);
-        testDisplay2.setText(convertToOctal);
-        testDisplay2.setTextSize(15);
-
-        String convertToHex = Conversions.decimalToHex(tempInput);
-        testDisplay3.setText(convertToHex);
-        testDisplay3.setTextSize(15);
-    }
-
-    private boolean checkIfValid() {
-        String check = seqInputContainer.getText().toString();
-        check = check.toUpperCase();
-        if (check.charAt(0) < '0' || check.charAt(0) > 'Z') {
-            return false;
-        } else if (check.charAt(0) > '9' && check.charAt(0) < 'A') {
-            return false;
-        }
-        return true;
     }
 }
